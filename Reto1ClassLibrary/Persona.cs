e0f6f8ed45a01069f44babf281534d08e5253065
Patxinho <santi.r.ortego@gmail.com>
2014-09-10 16:58:21 +0200

    Primera versión
    Programa funcional

---
+++ Reto1ClassLibrary/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reto1ClassLibrary
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Persona auxPersona = obj as Persona;
+
+            return this.Nombre == auxPersona.Nombre && this.Edad == auxPersona.Edad;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ Reto1ClassLibrary/Reto1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reto1ClassLibrary
+{
+    public class Reto1
+    {
+        //METODO PARA ORDENAR LA LISTA  (Linq)
+        //RECIBE UNA LIST<Persona>
+        //DEVUELVE UNA LIST<Persona>
+       public static List<Persona> OrdenarLista(List<Persona> personas){
+
+           if (personas != null)
+           {
+               return personas.OrderByDescending(x => x.Edad).ThenBy(x => x.Nombre).ToList();
+           }
+           else
+           {
+               return null;
+           }
+
+       }  
+    }
+}
